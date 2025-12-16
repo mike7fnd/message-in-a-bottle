d2eee780173f22f1964ecad7e69e0c113f65af9f
mike7fnd <mikefernandex227@gmail.com>
2025-12-16 20:08:42 +0800

    dark mode

--- src/ai/genkit.ts
+++
@@ -1,4 +0,0 @@
-'use server';
-/**
- * This file is no longer in use.
- */
--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-6364901469-82342",
-  "appId": "1:1046022135356:web:9fbaa658ef24fddb6cad39",
-  "apiKey": "AIzaSyAfR_F6Jeg1cLo5HsOX21R1JoLLpuSrwqA",
-  "authDomain": "studio-6364901469-82342.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1046022135356"
-};
